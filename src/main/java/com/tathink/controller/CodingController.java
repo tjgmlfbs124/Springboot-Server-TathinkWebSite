7de5089d6573cb5a71d3896fdc2361af5ef75c28
qwanchoi <chlrbdhks92@gmail.com>
2020-12-23 14:58:25 +0900

    컨트로러 생성

---
+++ src/main/java/com/tathink/controller/CodingController.java
@@ -0,0 +1,15 @@
+package com.tathink.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CodingController {
+	
+	@GetMapping("/coding")
+	public String codingPage(Model model) {
+		return "/coding/index";
+	}
+	
+}
