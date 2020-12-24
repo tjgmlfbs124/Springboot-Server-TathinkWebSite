a4c739ea047d45ae0aa6d37f1e3cf1a3fe0148ae
tjgmlfbs124 <tjgmlfbs124@gmail.com>
2020-12-24 10:49:50 +0900

    crossorigin 삭제

--- src/main/java/com/tathink/controller/CodingController.java
+++ src/main/java/com/tathink/controller/CodingController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CodingController {
-	
-	@CrossOrigin(origins= "http://tathink.cafe24.com:3000")
 	@GetMapping("/coding")
 	public String codingPage(Model model) {
 		return "/coding/index";
