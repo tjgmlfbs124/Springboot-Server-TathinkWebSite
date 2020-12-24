60ef246512558c6fb60ebf0421e9c5ec58650cc0
TAThink <tathink1001@gmail.com>
2020-12-24 10:03:53 +0900

    코딩교실 nodejs서버 연결중

--- src/main/java/com/tathink/controller/CodingController.java
+++ src/main/java/com/tathink/controller/CodingController.java
@@ -2,14 +2,15 @@ package com.tathink.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CodingController {
 	
+	@CrossOrigin(origins= "http://tathink.cafe24.com:3000")
 	@GetMapping("/coding")
 	public String codingPage(Model model) {
 		return "/coding/index";
 	}
-	
 }
